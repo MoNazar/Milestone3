cbed672407f1e1408d4d09f5722f77b465f7be5b
Maschamagin <35238192+Maschamagin@users.noreply.github.com>
2019-01-09 23:40:00 +0100

    Update VektorMatrix.h

--- VektorMatrix.h
+++ VektorMatrix.h
@@ -10,9 +10,3 @@ typedef vector<float> inputlayer;
 typedef vector <float> outputlayer;
 
 inputlayer il;
-
-using namespace std;
-
-class vektormatrix {
-	void kantengewichte(); // Eine Matrix als eingabe und ein Outputvektor (Layer). Die Matrix wird mit dem Layer multipliziert und es entsteht ein neues Inputvector 
-};
