25c61b14d5a5cc23b6ffcbf5e5f6e51daff9603d
MoNazar <44373397+MoNazar@users.noreply.github.com>
2019-01-12 06:53:20 +0100

    Create layer.h

---
+++ layer.h
@@ -0,0 +1,8 @@
+// Hier werden verschiedene art und weisen, ein Layer aufzustellen dargestellt. 
+
+typedef vector<double> Hiddenlayer;
+typedef vector<double> inputlayer;
+typedef vector <double> outputlayer;
+
+typedef vector<neuron> vectorneurons;
+
