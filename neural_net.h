2416d2ae30b48f6ad9a162d1b9094674f5e0c1d6
MoNazar <44373397+MoNazar@users.noreply.github.com>
2019-01-15 02:37:04 +0100

    Update neural_net.h

--- neural_net.h
+++ neural_net.h
@@ -1,53 +1,59 @@
-//
-// Creates a neural network based on a given topology.
-// stores neurons in vectors for every layer of the network.
-//
-
-#ifndef MILESTONE3_NEURAL_NET_H
-#define MILESTONE3_NEURAL_NET_H
-
 #include <vector>
 #include <math.h>
 #include "neuron.h"
 #include "weight.h"
+#include "layer.h"
+//#include "connection.h"
+
+#include <iostream>
 using namespace std;
 
 
 class neural_net {
 private:
-    vector<neuron> neurons_input_layer;
-    vector<neuron> neurons_hidden_layer;
-    vector<neuron> neurons_output_layer;
 
-    int number_of_hidden_layers;
-    int number_of_neurons_input_layer;
-    int number_of_neurons_hidden_layer;
-    int number_of_neurons_output_layer;
+	int number_of_hidden_layers;
+	int number_of_neurons_input_layer;
+	int number_of_neurons_hidden_layer;
+	int number_of_neurons_output_layer;
 
-    weight matrix_of_edgeweights_between_layers;
+	
 
 public:
+	inputlayer neurons_input_layer;
+	Hiddenlayer neurons_hidden_layer;
+    outputlayer neurons_output_layer;
+	weight matrix_of_edgeweights_between_layers;
+	weight matrix_of_edgeweights_between_hiddenlayer_and_outputlayer;
 
+	
     // getter
-    int get_number_of_hidden_layers();
-    int get_number_of_neurons_input_layer();
-    int get_number_of_neurons_hidden_layer();
-    int get_number_of_neurons_output_layer();
+	int get_number_of_hidden_layers();
+	int get_number_of_neurons_input_layer();
+	int get_number_of_neurons_hidden_layer();
+	int get_number_of_neurons_output_layer();
 
-    // setter
-    void set_number_of_hidden_layers(int number_of_hidden_layers);
-    void set_number_of_neurons_input_layer(int number_of_neurons_input_layer);
-    void set_number_of_neurons_hidden_layer(int number_of_neurons_hidden_layer);
-    void set_number_of_neurons_output_layer(int number_of_neurons_output_layer);
+	// setter
+	void set_number_of_hidden_layers(int number_of_hidden_layers);
+	void set_number_of_neurons_input_layer(int number_of_neurons_input_layer);
+	void set_number_of_neurons_hidden_layer(int number_of_neurons_hidden_layer);
+	void set_number_of_neurons_output_layer(int number_of_neurons_output_layer);
 
-    void set_up_network(vector<double> input_values);
-    
-    //generate Layer while using tpology
+	//generate Layer while using tpology
 	void generatelayer_inputlayer(inputlayer input);
 	void generatelayer_hiddenlayer(inputlayer input); 
 	void generatelayer_outputlayer(inputlayer input);
 
+	//get Layer 
+	void getlayer_input();
+	void getlayer_hidden();
+	void getlayer_output(); // returns all results of outputlayer, Aufgabe 2.b
+
+	//Connection of neurons
+	void generatematrix();
+	void connected(); // connection input_layer -> hidden_layer
+	void connected1(); // connection hidden_layer -> outputlayer
+	void generatematrix1();
+	void getmatrix();
+
 };
-
-
-#endif //MILESTONE3_NEURAL_NET_H
