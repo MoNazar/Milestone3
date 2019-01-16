b88fc661bea0420764500f98f63f9e6b5d9c6b9a
Maschamagin <35238192+Maschamagin@users.noreply.github.com>
2019-01-16 13:00:27 +0100

    Update layer.h

--- layer.h
+++ layer.h
@@ -1,5 +1,7 @@
-// Hier werden verschiedene art und weisen, ein Layer aufzustellen dargestellt. 
-
+// Hier werden verschiedene art und weisen, ein Layer aufzustellen dargestellt.
+#include <vector>
+#include "neuron.h"
+using namespace std;
 typedef vector<double> Hiddenlayer;
 typedef vector<double> inputlayer;
 typedef vector <double> outputlayer;
